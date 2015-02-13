64be54d2cc3a5e178ced168930beb84dc9fda983
ronscorp24 <ronscorp24@gmail.com>
2015-02-13 16:16:12 -0700

    TScan Updated
    Delay scanning program re-written as function

--- HomogeneousSystem/TScan/PlotMacro.m
+++ HomogeneousSystem/TScan/PlotMacro.m
@@ -1,12 +1,14 @@
+% clear all; clc;
+
 global Folder
 % Date = '2014_12_29';
 % Index = 'Run1';
 % Folder = strcat('./',Date,'/',Index,'/');
 Folder = './';
 
-TwoLevel_2D_0
-TwoLevel_2D_1
-TwoLevel_2D_2
-TwoLevel_2D_3
+TwoLevel_2D_Fn(0)
+TwoLevel_2D_Fn(1)
+TwoLevel_2D_Fn(2)
+TwoLevel_2D_Fn(3)
 CombineMat
 PlotSpecs
